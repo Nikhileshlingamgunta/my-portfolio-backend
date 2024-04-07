7f4d8b613633821c1c25a94cda0863b415f8d368
Nikhileshlingamgunta <124075195+Nikhileshlingamgunta@users.noreply.github.com>
2024-04-07 15:30:09 -0400

    Updated index.js file

--- index.js
+++ index.js
@@ -36,7 +36,4 @@ app.use(cors({
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
   }));
-app.use((req, res, next) => {
-console.log(`${req.method} ${req.url}`);
-next();
-});
+  
